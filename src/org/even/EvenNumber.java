474c0c752671a2fc202ca37db632e3476b69cb7b
logumakesh <logaveni@gmail.com>
2021-08-31 11:13:42 +0530

    brancha

---
+++ src/org/even/EvenNumber.java
@@ -0,0 +1,15 @@
+package org.even;
+
+public class EvenNumber {
+	public static void main(String[] args) {
+		int num =10;
+		if (num ==10) {
+			System.out.println("Even");
+			
+		} else {
+			System.out.println("odd");
+
+		}
+	}
+
+}
